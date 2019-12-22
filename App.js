f917848205ae8e31ef2a3b8b77a7e622f9fdc41f
jw <junweiwang@uqiauto.com>
2019-12-22 13:32:10 +0800

    底部导航实现

--- App.js
+++ App.js
@@ -1,114 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Text, StyleSheet, View, Image} from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedTab: 'popular',
+    };
+  }
+  changeTab = value => {
+    this.setState({
+      selectedTab: value,
+    });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'popular'}
+            title="最热"
+            selectedTitleStyle={{color: 'red'}}
+            renderIcon={() => (
+              <Image
+                source={require('./static/images/ic_polular.png')}
+                style={styles.tabIcon}></Image>
+            )}
+            renderSelectedIcon={() => (
+              <Image
+                source={require('./static/images/ic_polular.png')}
+                style={[styles.tabIcon, {tintColor: 'red'}]}></Image>
+            )}
+            onPress={() => {
+              this.changeTab('popular');
+            }}>
+            <View style={styles.page}>
+              <Text>首页</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'trending'}
+            title="趋势"
+            selectedTitleStyle={{color: 'red'}}
+            renderIcon={() => (
+              <Image
+                source={require('./static/images/ic_trending.png')}
+                style={styles.tabIcon}></Image>
+            )}
+            renderSelectedIcon={() => (
+              <Image
+                source={require('./static/images/ic_trending.png')}
+                style={[styles.tabIcon, {tintColor: 'red'}]}></Image>
+            )}
+            onPress={() => {
+              this.changeTab('trending');
+            }}>
+            <View style={styles.page}>
+              <Text>趋势页</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'favorite'}
+            title="收藏"
+            selectedTitleStyle={{color: 'red'}}
+            renderIcon={() => (
+              <Image
+                source={require('./static/images/ic_favorite.png')}
+                style={styles.tabIcon}></Image>
+            )}
+            renderSelectedIcon={() => (
+              <Image
+                source={require('./static/images/ic_favorite.png')}
+                style={[styles.tabIcon, {tintColor: 'red'}]}></Image>
+            )}
+            onPress={() => {
+              this.changeTab('favorite');
+            }}>
+            <View style={styles.page}>
+              <Text>收藏页</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'my'}
+            title="我的"
+            selectedTitleStyle={{color: 'red'}}
+            renderIcon={() => (
+              <Image
+                source={require('./static/images/ic_my.png')}
+                style={styles.tabIcon}></Image>
+            )}
+            renderSelectedIcon={() => (
+              <Image
+                source={require('./static/images/ic_my.png')}
+                style={[styles.tabIcon, {tintColor: 'red'}]}></Image>
+            )}
+            onPress={() => {
+              this.changeTab('my');
+            }}>
+            <View style={styles.page}>
+              <Text>我的</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </TabNavigator.Item>
+        </TabNavigator>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  page: {
+    flex: 1,
+    backgroundColor: 'red',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  tabIcon: {
+    width: 22,
+    height: 22,
   },
 });
-
-export default App;
--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+console.disableYellowBox = true; //屏蔽黄色警告
 AppRegistry.registerComponent(appName, () => App);
